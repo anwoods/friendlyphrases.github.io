ae2025b4a08f2e4edcde514fcb6f201975b46392
Shruthi Sankarram <ssankarram@ucdavis.edu>
2018-02-17 09:01:27 -0800

    starting functions

--- friendlyphrases.js
+++ friendlyphrases.js
@@ -1,8 +1,20 @@
 $( document ).ready(function()
 {
-
-    console.log('test!');
-    var afinn = afinn_en;
-    console.log(afinn);
-	//Tabletop.init( { key: latURL, callback: convertToGeoJSON, simpleSheet: true } );
+    var phrase = "I am bad whore";
+    console.log('starting..');
+    var afinn = afinn_en; //afinn english
+    var cursewords = cursewords_list; // cursewords 
+    var base_link = "http://words.bighugelabs.com/api/2/57fb326159dfcfd866e6778b33b451fb/"
+    get_negative_words(phrase);
 });
+
+/*
+    Returns a list of negative words
+*/
+function get_negative_words(phrase){
+    var phrase_words = phrase.split(" ");
+    for (var i = 0; i < phrase_words.length; i++){
+        console.log(phrase_words[i]);
+        console.log("afinn value: " + afinn[phrase_words[i]]);
+    }
+};
