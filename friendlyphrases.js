665590e3c1418f62852f6d493ddaea8990d59f85
Shruthi Sankarram <ssankarram@ucdavis.edu>
2018-02-17 08:40:37 -0800

    setup

--- friendlyphrases.js
+++ friendlyphrases.js
@@ -1,2 +1,8 @@
-var afinn = afinn_en;
-var x = afinn[obj];
+$( document ).ready(function()
+{
+
+    console.log('test!');
+    var afinn = afinn_en;
+    console.log(afinn);
+	//Tabletop.init( { key: latURL, callback: convertToGeoJSON, simpleSheet: true } );
+});
